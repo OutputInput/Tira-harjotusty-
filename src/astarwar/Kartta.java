7f26ee1898e6597767daf23b58a023aa899b8c00
pxkorpel <pxkorpel@dhp-20.cs.helsinki.fi>
2012-08-06 16:00:32 +0300

    tiedoston lukeminen ja tulostus

--- src/astarwar/Kartta.java
+++ src/astarwar/Kartta.java
@@ -4,35 +4,65 @@
  */
 package astarwar;
 
+import java.util.*;
 import java.io.*;
-import java.util.Scanner;
 
 /**
- * Testiä
+ * GT
+ *
  * @author pxkorpel
  */
 public class Kartta {
 
     private static Scanner lukija = new Scanner(System.in);
+    char[][] kartta = new char[4][4];
 
-    public void luetiedosto() throws FileNotFoundException {
-        
-        System.out.println("tiedoston nimi");        
+    public void luetiedostotaulukkoon() throws FileNotFoundException {
+
+        System.out.println("tiedoston nimi");
         String tiedostonNimi = lukija.nextLine();
         File tiedosto_olio = new File(tiedostonNimi);
         if (!tiedosto_olio.exists()) {
             System.out.println("Tiedostoa " + tiedostonNimi + " ei löydy!");
             return; // keskeytetään kaikki!
         }
+
         Scanner syöttötiedosto = new Scanner(tiedosto_olio);
         while (syöttötiedosto.hasNextLine()) {
-            String rivi = syöttötiedosto.nextLine();
-            System.out.println(rivi);
+
+            for (int n = 0; n < 3; n++) {
+                String rivi = syöttötiedosto.nextLine();
+                for (int i = 0; i < rivi.length(); i++) {
+                    kartta[n][i] = rivi.charAt(i);
+                }
+            }
         }
         syöttötiedosto.close(); // Huom.!!
     }
-    
-    
-    
-    
+
+    public void tulostakartta(char[][] kartta) throws FileNotFoundException {
+        System.out.println("");
+
+        for (int monesrivi = 0; monesrivi < 4; monesrivi++) {
+            for (int n = 0; n < 4; n++) {
+                System.out.print(kartta[monesrivi][n]);
+            }
+            System.out.println("");
+        }
+    }
+
+    public void tulostareitti() throws FileNotFoundException {
+    }
+
+    public void arvotakartta() {
+
+        int[][] arvokartta = new int[4][4];
+
+        for (int n = 0; n < 4; n++) {
+            for (int i = 0; i < 4; i++) {
+                arvokartta[n][i] = 10000;
+            }
+        }
+
+    }
 }
