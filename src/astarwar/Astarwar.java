c1d255d603e48731fec901f2ac3fe4113775ac0d
pxkorpel <pxkorpel@dhp-20.cs.helsinki.fi>
2012-08-06 13:56:17 +0300

    eka

---
+++ src/astarwar/Astarwar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package astarwar;
+
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author pxkorpel
+ */
+public class Astarwar {
+        /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        
+       Kartta kartta = new Kartta();
+       kartta.luetiedosto();
+   }
+}
---
+++ src/astarwar/Kartta.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package astarwar;
+
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author pxkorpel
+ */
+public class Kartta {
+
+    private static Scanner lukija = new Scanner(System.in);
+
+    public void luetiedosto() throws FileNotFoundException {
+        
+        System.out.println("tiedoston nimi");        
+        String tiedostonNimi = lukija.nextLine();
+        File tiedosto_olio = new File(tiedostonNimi);
+        if (!tiedosto_olio.exists()) {
+            System.out.println("Tiedostoa " + tiedostonNimi + " ei löydy!");
+            return; // keskeytetään kaikki!
+        }
+        Scanner syöttötiedosto = new Scanner(tiedosto_olio);
+        while (syöttötiedosto.hasNextLine()) {
+            String rivi = syöttötiedosto.nextLine();
+            System.out.println(rivi);
+        }
+        syöttötiedosto.close(); // Huom.!!
+    }
+    
+    
+    
+    
+}
