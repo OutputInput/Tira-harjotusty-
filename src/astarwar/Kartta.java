39461ae9dcbe90c62dcfb65341dceda76282a41e
pxkorpel <pxkorpel@dhp-20.cs.helsinki.fi>
2012-08-06 17:00:06 +0300

    tiedoston lukeminen ja tulostus 2 kartan koko

--- src/astarwar/Kartta.java
+++ src/astarwar/Kartta.java
@@ -15,7 +15,9 @@ import java.io.*;
 public class Kartta {
 
     private static Scanner lukija = new Scanner(System.in);
-    char[][] kartta = new char[4][4];
+    int kartankoko = 10;
+    char[][] kartta = new char[kartankoko][kartankoko];
+    char[][] reittikartta = new char[kartankoko][kartankoko];
 
     public void luetiedostotaulukkoon() throws FileNotFoundException {
 
@@ -28,9 +30,11 @@ public class Kartta {
         }
 
         Scanner syöttötiedosto = new Scanner(tiedosto_olio);
+        Scanner syöttötiedosto2 = new Scanner(tiedosto_olio);
+        String rivi1 = syöttötiedosto2.nextLine();
         while (syöttötiedosto.hasNextLine()) {
 
-            for (int n = 0; n < 3; n++) {
+            for (int n = 0; n < rivi1.length(); n++) {
                 String rivi = syöttötiedosto.nextLine();
                 for (int i = 0; i < rivi.length(); i++) {
                     kartta[n][i] = rivi.charAt(i);
@@ -38,31 +42,56 @@ public class Kartta {
             }
         }
         syöttötiedosto.close(); // Huom.!!
+        syöttötiedosto2.close();
     }
 
     public void tulostakartta(char[][] kartta) throws FileNotFoundException {
         System.out.println("");
 
-        for (int monesrivi = 0; monesrivi < 4; monesrivi++) {
-            for (int n = 0; n < 4; n++) {
+        for (int monesrivi = 0; monesrivi < kartankoko; monesrivi++) {
+            for (int n = 0; n < kartankoko; n++) {
                 System.out.print(kartta[monesrivi][n]);
             }
             System.out.println("");
         }
     }
 
-    public void tulostareitti() throws FileNotFoundException {
-    }
+    public void arvotareitti(char[][] kartta) throws FileNotFoundException {
+        reittikartta = uusikartta(kartta);
+        int[][] arvokartta = arvotakartta(kartta);
 
-    public void arvotakartta() {
-
-        int[][] arvokartta = new int[4][4];
-
-        for (int n = 0; n < 4; n++) {
-            for (int i = 0; i < 4; i++) {
-                arvokartta[n][i] = 10000;
+        for (int n = 0; n < kartankoko; n++) {
+            for (int i = 0; i < kartankoko; i++) {
+                if (arvokartta[n][i] < 2) {
+                    reittikartta[n][i] = 'o';
+                }
             }
         }
+    }
 
+    public char[][] uusikartta(char[][] kartta) {
+
+        char[][] kartta2 = new char[kartankoko][kartankoko];
+        for (int n = 0; n < kartankoko; n++) {
+            for (int i = 0; i < kartankoko; i++) {
+                kartta2[n][i] = kartta[n][i];
+            }
+        }
+        return kartta2;
+    }
+
+    public int[][] arvotakartta(char[][] kartta) {
+
+        int[][] arvokartta = new int[kartankoko][kartankoko];
+        for (int n = 0; n < kartankoko; n++) {
+            for (int i = 0; i < kartankoko; i++) {
+                if (kartta[n][i] == '#') {
+                    arvokartta[n][i] = 10000;
+                } else {
+                    arvokartta[n][i] = 1;
+                }
+            }
+        }
+        return arvokartta;
     }
 }
