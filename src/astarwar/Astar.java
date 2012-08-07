60185a415bae783262126766707c9c9aca1a8ec2
pxkorpel <pxkorpel@dhp-20.cs.helsinki.fi>
2012-08-07 13:00:36 +0300

    A* - arvioituetäisyys

--- src/astarwar/Astar.java
+++ src/astarwar/Astar.java
@@ -11,17 +11,34 @@ package astarwar;
 public class Astar {
 
     Kartta kartta;
+    int[] käydyt;
+    int[] viereiset;
 
     public Astar(Kartta kartta) {
         this.kartta = kartta;
     }
 
-    public int arvioituetäisyys() {
-        int pistex = kartta.alkupistex - kartta.loppupistex;
-        int pistey = kartta.alkupistey - kartta.loppupistey;
+    public int arvioitumatkamaaliin(int x, int y) {
+        //H
+        int pistex = x - kartta.loppupistex;
+        int pistey = y - kartta.loppupistey;
         int summa = (int) Math.pow(pistex, 2);
         summa = summa + (int) Math.pow(pistey, 2);
         int arvioituetäisyys = (int) Math.sqrt(summa);
         return arvioituetäisyys;
+    }
+
+    public int matkatähänasti() {
+        //G
+        return 4;
+    }
+
+    public void viereiset() {
+    }
+
+    public int F(int x, int y) {
+        //F = G + H
+        int F = arvioitumatkamaaliin(x, y) + matkatähänasti();
+        return F;
     }
 }
