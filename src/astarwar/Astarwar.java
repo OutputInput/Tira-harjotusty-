d18a0b4d93f2f27e21755cadb24e381758fdb48a
pxkorpel <pxkorpel@dhp-20.cs.helsinki.fi>
2012-08-07 17:59:36 +0300

    hyvä

--- src/astarwar/Astarwar.java
+++ src/astarwar/Astarwar.java
@@ -17,15 +17,23 @@ public class Astarwar {
      */
     public static void main(String[] args) throws FileNotFoundException {
 
+        //nää kolme pitää olla ensin
         Kartta kartta = new Kartta();
         kartta.luetiedostotaulukkoon();
         kartta.arvotakartta(kartta.kartta);
+
+        //sitten voi luoda Astar:n
         Astar A = new Astar(kartta);
+
         kartta.arvotareitti(kartta.kartta);
         kartta.tulostakartta(kartta.kartta);
         kartta.tulostakartta(kartta.reittikartta);
         A.päivitänaapurit(A.alkusolmu);
         System.out.println(A.arvioitumatkamaaliin(A.alkusolmu.x, A.alkusolmu.y));
+
+        System.out.println(A.solmujono.size());
+
+        System.out.println("wtf menee x ja y akselit sekasin");
 
     }
 }
