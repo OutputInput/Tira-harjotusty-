74827ef31134c26a44b4a180bf6488fcb6a52edd
pxkorpel <pxkorpel@dhp-20.cs.helsinki.fi>
2012-08-07 15:30:18 +0300

    priority queue ja lisää naapurit solmujonoon

--- src/astarwar/Astar.java
+++ src/astarwar/Astar.java
@@ -4,6 +4,8 @@
  */
 package astarwar;
 
+import java.util.PriorityQueue;
+
 /**
  *
  * @author pxkorpel
@@ -12,11 +14,12 @@ public class Astar {
 
     Kartta kartta;
     int[] käydyt;
-
+    Solmu alkusolmu;
+    PriorityQueue<Solmu> solmujono;
 
     public Astar(Kartta kartta) {
         this.kartta = kartta;
-        
+        this.solmujono = new PriorityQueue<Solmu>();
     }
 
     public int arvioitumatkamaaliin(int x, int y) {
@@ -35,17 +38,55 @@ public class Astar {
         return 4;
     }
 
-    public void päivitänaapurit() {
+    public void päivitänaapurit(Solmu tämänhetkinensolmu) {
+
+        int x = tämänhetkinensolmu.x + 1;
+        int y = tämänhetkinensolmu.y;
+        Solmu vanhempi = tämänhetkinensolmu.vanhempi;
+        int etäisyys = tämänhetkinensolmu.arvioituetäisyys;
+        if (!kartta.onkoseinä(x, y)) {
+            lisäänaapurinaapureihin(x, y, vanhempi, etäisyys);
+        }
+
+        x = tämänhetkinensolmu.x - 1;
+        y = tämänhetkinensolmu.y;
+        vanhempi = tämänhetkinensolmu.vanhempi;
+        etäisyys = tämänhetkinensolmu.arvioituetäisyys;
+        if (!kartta.onkoseinä(x, y)) {
+            lisäänaapurinaapureihin(x, y, vanhempi, etäisyys);
+        }
+
+        x = tämänhetkinensolmu.x;
+        y = tämänhetkinensolmu.y - 1;
+        vanhempi = tämänhetkinensolmu.vanhempi;
+        etäisyys = tämänhetkinensolmu.arvioituetäisyys;
+        if (!kartta.onkoseinä(x, y)) {
+            lisäänaapurinaapureihin(x, y, vanhempi, etäisyys);
+        }
+
+        x = tämänhetkinensolmu.x;
+        y = tämänhetkinensolmu.y + 1;
+        vanhempi = tämänhetkinensolmu.vanhempi;
+        etäisyys = tämänhetkinensolmu.arvioituetäisyys;
+        if (!kartta.onkoseinä(x, y)) {
+            lisäänaapurinaapureihin(x, y, vanhempi, etäisyys);
+        }
+
+
     }
 
     public int F(int x, int y) {
         //F = G + H
         int F = arvioitumatkamaaliin(x, y) + matkatähänasti();
         return F;
-    } 
-   
-    public void lähinnaapuri(){
-        
     }
-    
+
+    public void lähinnaapuri() {
+    }
+
+    public void lisäänaapurinaapureihin(int x, int y, Solmu vanhempi, int etäisyys) {
+        Solmu S = new Solmu(x, y, vanhempi, etäisyys);
+        solmujono.add(S);
+
+    }
 }
