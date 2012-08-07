8f19366cb395cc33094b55e12521157b7e4e59ef
pxkorpel <pxkorpel@dhp-20.cs.helsinki.fi>
2012-08-07 16:24:52 +0300

    hyvä

--- src/astarwar/Solmu.java
+++ src/astarwar/Solmu.java
@@ -8,12 +8,18 @@ package astarwar;
  *
  * @author pxkorpel
  */
-public class Solmu {
+public class Solmu implements Comparable<Solmu> {
 
-    int x;
-    int y;
+    int x = 0;
+    int y = 0;
     Solmu vanhempi;
     int arvioituetäisyys; //H 
+
+    public void Solmu(int x, int y, int arvioituetäisyys) {
+        this.x = x;
+        this.y = y;
+        this.arvioituetäisyys = arvioituetäisyys;
+    }
 
     public Solmu(int x, int y, Solmu vanhempi, int arvioituetäisyys) {
         this.x = x;
@@ -25,5 +31,16 @@ public class Solmu {
     public Solmu annavanhempi() {
 
         return this.vanhempi;
+    }
+
+    @Override
+    public int compareTo(Solmu s) {
+        if (this.arvioituetäisyys == s.arvioituetäisyys) {
+        return 0;
+        } else if (this.arvioituetäisyys > s.arvioituetäisyys) {
+            return 1;
+        } else {
+            return -1;
+        }
     }
 }
