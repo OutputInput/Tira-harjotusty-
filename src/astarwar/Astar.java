d01e933761380ea3367f2cf8568c02575763508b
pxkorpel <pxkorpel@dhp-20.cs.helsinki.fi>
2012-08-09 14:02:22 +0300

    akselit korjattu

--- src/astarwar/Astar.java
+++ src/astarwar/Astar.java
@@ -25,7 +25,6 @@ public class Astar {
         int alkupistex = kartta.alkupistex;
         int alkupistey = kartta.alkupistey;
         int matka = this.arvioitumatkamaaliin(alkupistex, alkupistey);
-
         alkusolmu = new Solmu(kartta.alkupistex, kartta.alkupistey, null, arvioitumatkamaaliin(alkupistex, alkupistey));
     }
 
@@ -73,12 +72,6 @@ public class Astar {
 
         x = tämänhetkinensolmu.x;
         y = tämänhetkinensolmu.y + 1;
-        System.out.println(x);
-        System.out.println(y);
-        char mato = kartta.kartta[2][4];
-        System.out.println(mato);
-        mato = kartta.kartta[4][2];
-        System.out.println(mato);
         vanhempi = tämänhetkinensolmu.vanhempi;
         etäisyys = tämänhetkinensolmu.arvioituetäisyys;
         if (!kartta.onkoseinä(x, y)) {
